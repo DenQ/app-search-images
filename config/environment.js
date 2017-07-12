bb8fc04d3fb218f8682c8ae623d93ccbd78dd1ac
DenQ <denquick@gmail.com>
2017-07-12 18:04:11 +0300

    Setting pods

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'app-search-images',
+    podModulePrefix: 'app-search-images/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
