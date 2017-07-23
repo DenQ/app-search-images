1046880f8da2942a5bf0e47f9e27e87dd7e897a4
DenQ <denquick@gmail.com>
2017-07-23 19:32:01 +0300

    Setting adapter for searching

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
   host: 'https://api.flickr.com/services/rest',
 
-  queryRecord(store, type, query) {
+  query(store, type, query) {
     return new Ember.RSVP.Promise((resolve, reject) => {
       Ember.$.getJSON(this.get('host'), query).then(function(data) {
         resolve(data);
