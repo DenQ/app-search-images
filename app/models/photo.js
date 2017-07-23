fee620cf46fe3d031cd607fbb1d3c7e5113d05f5
DenQ <denquick@gmail.com>
2017-07-23 20:00:36 +0300

    Card. Model photo

--- app/models/photo.js
+++ app/models/photo.js
@@ -1,5 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  title: DS.attr(),
+  description: DS.attr(),
+  sizes: DS.attr(),
+  comments: DS.attr(),
+  views: DS.attr('number'),
+  count_faves: DS.attr('number'),
 
+
+  lastSize: Ember.computed('sizes', function() {
+    const sizes = this.get('sizes.size');
+    return Ember.A(sizes).get('lastObject');
+  }),
 });
