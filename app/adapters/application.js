d59082f28f3906120ca5dc3f69d57f5e42451b5c
DenQ <denquick@gmail.com>
2017-08-11 12:08:53 +0300

    Refactoring. Adapters

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,25 +1,20 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
+function request(store, type, query) {
+  return new Ember.RSVP.Promise((resolve, reject) => {
+    const host = this.get('host');
+    Ember.$.getJSON(host, query).then(function(data) {
+      resolve(data);
+    }, function(jqXHR) {
+      reject(jqXHR);
+    });
+  });
+}
+
 export default DS.RESTAdapter.extend({
   host: 'https://api.flickr.com/services/rest',
+  queryRecord: request,
+  query: request,
 
-  query(store, type, query) {
-    return new Ember.RSVP.Promise((resolve, reject) => {
-      Ember.$.getJSON(this.get('host'), query).then(function(data) {
-        resolve(data);
-      }, function(jqXHR) {
-        reject(jqXHR);
-      });
-    });
-  },
-  queryRecord(store, type, query) {
-    return new Ember.RSVP.Promise((resolve, reject) => {
-      Ember.$.getJSON(this.get('host'), query).then(function(data) {
-        resolve(data);
-      }, function(jqXHR) {
-        reject(jqXHR);
-      });
-    });
-  }
 });
