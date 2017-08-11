b5d9e2841d2a6350710a00f913efb81448c2abf7
DenQ <denquick@gmail.com>
2017-08-11 15:01:46 +0300

    This close #33

--- app/pods/components/interactive/lists/comments-list/component.js
+++ app/pods/components/interactive/lists/comments-list/component.js
@@ -1,13 +1,34 @@
 import Ember from 'ember';
 import computedReverse from 'app-search-images/libs/computed/reverse';
 
-// const { computed } = Ember;
+const { computed } = Ember;
 
 export default Ember.Component.extend({
   tagName: 'ul',
   classNames: ['list-group'],
 
-  modelReversed: computedReverse('datecreate'),
-  // model10:
+  modeFull: false,
+  more10: computed.gt('model.length', 10),
+  isEmpty: computed.empty('model'),
 
+  modelReversed: computedReverse('datecreate'),
+  model10: computed('modelReversed', function() {
+    const model = this.get('modelReversed');
+    return model.slice(0, 10);
+  }),
+  modelResult: computed('model', 'modeFull', function() {
+    const modeFull = this.get('modeFull');
+    return modeFull ? this.get('modelReversed') : this.get('model10');
+  }),
+  showButton: computed('more10', 'modeFull', function() {
+    const modeFull = this.get('modeFull');
+    const more10 = this.get('more10');
+    return !modeFull && more10;
+  }),
+
+  actions: {
+    showMore() {
+      this.set('modeFull', true);
+    },
+  },
 });
