74483d5f73283cd6e834cb05f4a7ccceeb532b48
DenQ <denquick@gmail.com>
2017-08-11 13:24:46 +0300

    Refactoring. Extract computedReverse to libs

--- app/pods/components/interactive/lists/comments-list/component.js
+++ app/pods/components/interactive/lists/comments-list/component.js
@@ -1,23 +1,13 @@
 import Ember from 'ember';
+import computedReverse from 'app-search-images/libs/computed/reverse';
 
-const { computed } = Ember;
+// const { computed } = Ember;
 
 export default Ember.Component.extend({
   tagName: 'ul',
   classNames: ['list-group'],
 
-  modelReversed: computed.sort('model', function(a, b) {
-    const first = Number(a.get('datecreate'));
-    const two = Number(b.get('datecreate'));
-    if (first < two) {
-      return 1;
-    } else if (first > two) {
-      return -1;
-    } else {
-      return 0;
-    }
-  }),
-
+  modelReversed: computedReverse('datecreate'),
   // model10:
 
 });
