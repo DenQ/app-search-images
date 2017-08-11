688655cded50b3719a492b399fb1b36123c0b599
DenQ <denquick@gmail.com>
2017-08-11 13:17:36 +0300

    Reversed model items

--- app/pods/components/interactive/lists/comments-list/component.js
+++ app/pods/components/interactive/lists/comments-list/component.js
@@ -1,7 +1,23 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
   tagName: 'ul',
   classNames: ['list-group'],
-  
+
+  modelReversed: computed.sort('model', function(a, b) {
+    const first = Number(a.get('datecreate'));
+    const two = Number(b.get('datecreate'));
+    if (first < two) {
+      return 1;
+    } else if (first > two) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }),
+
+  // model10:
+
 });
--- tests/integration/pods/components/interactive/lists/comments-list/component-test.js
+++ tests/integration/pods/components/interactive/lists/comments-list/component-test.js
@@ -6,20 +6,6 @@ moduleForComponent('interactive/lists/comments-list', 'Integration | Component |
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{interactive/lists/comments-list}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#interactive/lists/comments-list}}
-      template block text
-    {{/interactive/lists/comments-list}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'more...');
 });
