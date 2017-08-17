60192749555b333eb3b1bdc8e8beb44de454303f
DenQ <denquick@gmail.com>
2017-08-17 20:56:25 +0300

    Show active color

--- app/pods/components/interactive/filters/color-filter/component.js
+++ app/pods/components/interactive/filters/color-filter/component.js
@@ -5,6 +5,19 @@ export default Ember.Component.extend({
   colors,
   setColors: '',
 
+  activeColors: Ember.computed('colors', 'activeColorCodes', function() {
+    const colors = this.get('colors');
+    const activeColorCodesString = this.get('activeColorCodes');
+    const activeColorCodesCollection = activeColorCodesString.split(',');
+    return colors.map((item) => {
+      activeColorCodesCollection.forEach((color) => {
+        const isActive = String(color) === String(item.id);
+        Ember.set(item, 'isActive', isActive);
+      });
+      return item;
+    });
+  }),
+
   actions: {
     setColor(color) {
       const { id } = color;
