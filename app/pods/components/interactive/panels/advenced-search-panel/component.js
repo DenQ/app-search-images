eb04c66a475f42f7b1d3d132104246f28d75cde8
DenQ <denquick@gmail.com>
2017-08-18 20:33:20 +0300

    Styles. Refactoring

--- app/pods/components/interactive/panels/advenced-search-panel/component.js
+++ app/pods/components/interactive/panels/advenced-search-panel/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['advenced-search-panel'],
 
   fullNameChanged: Ember.observer('sort', function() {
     const sort = this.get('sort');
