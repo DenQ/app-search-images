7a9f94ab850aaccca46ff608dbdb77edc500c656
DenQ <denquick@gmail.com>
2017-08-18 11:10:34 +0300

    Fix bug with isActive color

--- app/pods/components/interactive/filters/color-filter/component.js
+++ app/pods/components/interactive/filters/color-filter/component.js
@@ -3,17 +3,15 @@ import colors from 'app-search-images/libs/const/color-schema';
 
 export default Ember.Component.extend({
   colors,
-  setColors: '',
 
   activeColors: Ember.computed('colors', 'activeColorCodes', function() {
     const colors = this.get('colors');
     const activeColorCodesString = this.get('activeColorCodes') || '';
     const activeColorCodesCollection = activeColorCodesString.split(',');
+
     return colors.map((item) => {
-      activeColorCodesCollection.forEach((color) => {
-        const isActive = String(color) === String(item.id);
-        Ember.set(item, 'isActive', isActive);
-      });
+      const isActive = activeColorCodesCollection.includes(String(item.id));
+      Ember.set(item, 'isActive', isActive);
       return item;
     });
   }),
