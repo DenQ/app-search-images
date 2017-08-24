34ca1ebde8d0b6b11b8701cba049b85b172bd7b3
DenQ <denquick@gmail.com>
2017-08-24 14:49:14 +0300

    Computed size photo

--- app/pods/components/interactive/modals/photo-modal/component.js
+++ app/pods/components/interactive/modals/photo-modal/component.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sizeClass: Ember.computed('model.width', function() {
+    const width = Number(this.get('model.width'));
+    if (width < 600) {
+      return '';
+    } else if (width < 1000) {
+      return 'lg'
+    } else if (width < 1200) {
+      return 'lgx';
+    } else {
+      return 'lgxx';
+    }
+  }),
   actions: {
     onHide() {
       this.set('open', false);
