dbb9160e508e4b4730a8c533f845349364675229
DenQ <denquick@gmail.com>
2017-08-24 15:08:55 +0300

    This close #30. Set title modal

--- app/pods/components/interactive/modals/photo-modal/component.js
+++ app/pods/components/interactive/modals/photo-modal/component.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  title: Ember.computed('model.source', function() {
+    const model = this.get('model');
+    if (model) {
+      const { width, height } = model;
+      return `Show image (${width} x ${height})`;
+    }
+  }),
   sizeClass: Ember.computed('model.width', function() {
     const width = Number(this.get('model.width'));
     if (width < 600) {
